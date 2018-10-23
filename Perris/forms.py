b7bcadd2d7883e441eb3f04705a33befdfb53b76
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2018-10-23 13:11:45 -0300

    actualización FormRegistroCliente

--- Perris/forms.py
+++ Perris/forms.py
@@ -1,7 +1,41 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Div
+from .models import Cliente
 
-class FormRegistroCliente(forms.Form):
-	run=forms.CharField(widget=forms.TextInput(),label="RUN",required=True)
+class FormRegistroCliente(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        # specify what fields should be used in this form.
+        fields = ('run',
+                  'nombre', 'apellido',
+                  'telefono',
+                 )
+
+    def __init__(self, *args, submit_title="儲存編輯", **kwargs):
+        super().__init__(*args, **kwargs)
+        my_field_text= [
+            # (field_name, Field title label, Detailed field description)
+            ('run', 'run', 'run de cliente'),
+            ('nombre', 'nombre', 'nombre cliente'),
+            ('apellido', 'apellido', 'apellido clienjte'),
+            ('telefono', '', ''),
+            
+         ]  
+    # Set layout for fields.
+        self.helper=FormHelper()
+        self.helper.layout = Layout(
+            Div('run', 'nombre'),
+            Div(
+                Div('apellido', css_class="col-sm-2"),
+                Div('telefono', css_class="col-sm-10"),
+                css_class = 'row'
+            )
+        )           
+        self.helper.add_input(Submit('submit', submit_title))
+
+class FormRegistroClienteDos(forms.Form):
+	run=forms.CharField(widget=forms.TextInput(attrs={'class':'input'}),label="RUN",required=True)
 	nombre=forms.CharField(widget=forms.TextInput(),required=True)
 	apellido=forms.CharField(widget=forms.TextInput(),required=True)
 	mail=forms.EmailField(widget=forms.EmailInput(),required=True)
