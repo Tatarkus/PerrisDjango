304210fe0f0f36e0a92c13635dd3a15c54fbe199
Tatarkus <matias.trks@gmail.com>
2018-10-24 14:51:07 -0300

    eliminado caracter basura

--- Perris/urls.py
+++ Perris/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns=[
-    url(r'^registro$',views.registro, name="registro"),
-    url(r'^registrar$',views.registrar, name="registrar"),
-    url(r'^login$',views.ingresar, name="login"),
-    url(r'^mascota$',views.mascota, name="mascotas"),    
-    url(r'^adopcion$',views.adopcion, name="adopcion"),   
-    url('index',views.index, name="index"), 
+	url(r'^registro$',views.registro, name="registro"),
+	url(r'^registrar$',views.registrar, name="registrar"),
+	url(r'^login$',views.ingresar, name="login"),
+	url(r'^mascota$',views.mascota, name="mascotas"),    
+	url(r'^adopcion$',views.adopcion, name="adopcion"),   
+	url('index',views.index, name="index"),
 	url('inicio',views.inicio, name="inicio"), 
 	url('clientes',views.clientes, name="clientes"), 
+    url(r'^$',views.inicio, name="inicio"),
     url('^', include('django.contrib.auth.urls')), #necesario para el password reset
     url(r'^accounts/login/$',views.index,name="inicio"), #redireccion apropiada, django por defecto te envia a esa url
+]
