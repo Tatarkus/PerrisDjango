4713ec3c26f7f761d315edd2b48bed817a37a32f
Tatarkus <matias.trks@gmail.com>
2018-10-24 14:46:41 -0300

    resuelto conflicto con los scripts
    no dejar una urls que tome cualquier dirección y la lleve al index

--- Perris/urls.py
+++ Perris/urls.py
@@ -9,12 +9,6 @@ urlpatterns=[
     url(r'^adopcion$',views.adopcion, name="adopcion"),   
     url('index',views.index, name="index"), 
 	url('inicio',views.inicio, name="inicio"), 
-<<<<<<< HEAD
 	url('clientes',views.clientes, name="clientes"), 
-
-=======
-    url(r'^$',views.inicio, name="inicio"),
     url('^', include('django.contrib.auth.urls')), #necesario para el password reset
     url(r'^accounts/login/$',views.index,name="inicio"), #redireccion apropiada, django por defecto te envia a esa url
->>>>>>> cfc1f0e82c3b1e32089cf6dc1708f7a2218a5e4a
-]
