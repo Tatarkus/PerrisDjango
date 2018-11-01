dd3562562187a8b1db1fa09cab98ae4debcf6f72
Matias Fuentes <matias.trks@gmail.com>
2018-11-01 13:45:26 -0300

    formulario de registro
    Cambiado el diseño del forumalrio de registro.
    Ya no se ocupa el formulario rapido extendiendo de modelforms porque había que usar 2 formularios uno para User y otro para Cliente.
    Ahora estendemos de Form no más, menos elegante pero más fácil de manejar. El manejo del modelo se controla en las vistas.

--- Perris/forms.py
+++ Perris/forms.py
@@ -4,32 +4,42 @@ from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Div
 from .models import Cliente
 from django.contrib.auth.models import User
 
-class FormRegistroCliente(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        # specify what fields should be used in this form.
-        fields = ('run',
-                  'nombre', 'apellido','email',
-                  'telefono',
-                 )
+class FormRegistroCliente(forms.Form):
+
+    run=forms.CharField(widget=forms.TextInput(),label="RUN")
+    nombre=forms.CharField(widget=forms.TextInput(),label="Nombre Usuario")
+    apellido=forms.CharField(widget=forms.TextInput(),label="Apellido")
+    mail=forms.EmailField(widget=forms.TextInput(),label="Correo Electronico")
+    telefono=forms.CharField(widget=forms.TextInput(),label="Telefono")
+    username=forms.CharField(widget=forms.TextInput(),label="Usuario")
+    password=forms.CharField(widget=forms.PasswordInput(),label="Contraseña")
+    password2=forms.CharField(widget=forms.PasswordInput(),label="Confirmar Contraseña")
+
 
     def __init__(self, *args, submit_title="Enviar", **kwargs):
         super().__init__(*args, **kwargs)
-        my_field_text= [
-            # (field_name, Field title label, Detailed field description)
-            ('run', 'run', 'run de cliente'),
-            ('nombre', 'nombre', 'nombre cliente'),
-            ('apellido', 'apellido', 'apellido clienjte'),
-            ('telefono', '', ''),
-            
-         ]  
-    # Set layout for fields.
+
         self.helper=FormHelper()
+        self.fields['password'].help_text = "La contraseña debe contener mínimo 4 caracteres."
         self.helper.layout = Layout(
-            Div('run', 'nombre'),
             Div(
-                Div('apellido', css_class="col-sm-2"),
-                Div('telefono', css_class="col-sm-10"),
+                Div('run', css_class="col-sm-6"),
+                Div('mail', css_class="col-sm-6"),
+                css_class = 'row'
+            ),
+            Div(
+                Div('nombre', css_class="col-sm-6"),
+                Div('apellido', css_class="col-sm-6"),
+                css_class = 'row'
+            ),
+            Div(
+                Div('username', css_class="col-sm-6"),
+                Div('telefono', css_class="col-sm-6"),
+                css_class = 'row'
+            ),
+            Div(
+                Div('password', css_class="col-sm-6"),
+                Div('password2', css_class="col-sm-6"),
                 css_class = 'row'
             )
         )           
